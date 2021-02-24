9cb806f31eb42c8209f2e57e13f40e5c210889f1
U-WangE <52265130+U-WangE@users.noreply.github.com>
2021-02-25 01:18:38 +0900

    No.11654 (Baekjoon)

---
+++ 11654.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        char n = new Scanner(System.in).next().charAt(0);
+        
+        System.out.println((int)n);
+    }
+}
