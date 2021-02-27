49773ecf16b159f17f4f29999c57200cc6145e35
U-WangE <52265130+U-WangE@users.noreply.github.com>
2021-02-28 00:28:16 +0900

    No.2742 (Baekjoon)

---
+++ 2742_ver.1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int n = new Scanner(System.in).nextInt();
+        
+        for(int i = n; i > 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
