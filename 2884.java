61be987feb7fe62363849c685c5eafa8e8c5a463
U-WangE <52265130+U-WangE@users.noreply.github.com>
2021-02-28 00:41:25 +0900

    No.2884 (Baekjoon)

---
+++ 2884.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int h = sc.nextInt();
+        int m = sc.nextInt();
+        int t = h*60 + m - 45;
+        
+        if(t < 0)
+            t += 24*60;
+        System.out.print(t/60 + " " + t%60);
+    }
+}
